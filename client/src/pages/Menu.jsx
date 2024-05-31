554017047c052a0f7c93cbe3c9df19340e179d88
milangautamDemon <milangautam071440304@gmail.com>
2024-05-31 15:40:29 +0545

    updated: menu item filter

--- client/src/pages/Menu.jsx
+++ client/src/pages/Menu.jsx
@@ -5,7 +5,97 @@ import buffSteamMomo from "../assets/images/sea-momo.png"
 import QrScanner from "../components/common/QrScanner";
 import UniqueTitle from "../components/common/UniqueTitle";
 
+
+const menuItems = [
+  {
+    category: "buff",
+    menuImg : buffSteamMomo,
+    menuName : "buff Steam Momo",
+    itemDescription : "Freshly Stream Buff Momos made from whole wheat and kneaded with beta Carotena Rich Vegetable Juice",
+    menuPrice : "200"
+  },
+  {
+    category: "buff",
+    menuImg : buffSteamMomo,
+    menuName : "buff fry Momo",
+    itemDescription : "Freshly Stream Buff Momos made from whole wheat and kneaded with beta Carotena Rich Vegetable Juice",
+    menuPrice : "200"
+  },
+  {
+    category: "buff",
+    menuImg : buffSteamMomo,
+    menuName : "buff chilly Momo",
+    itemDescription : "Freshly Stream Buff Momos made from whole wheat and kneaded with beta Carotena Rich Vegetable Juice",
+    menuPrice : "200"
+  },
+  {
+    category: "chicken",
+    menuImg : buffSteamMomo,
+    menuName : "chicken Steam Momo",
+    itemDescription : "Freshly Stream Buff Momos made from whole wheat and kneaded with beta Carotena Rich Vegetable Juice",
+    menuPrice : "200"
+  },
+  {
+    category: "chicken",
+    menuImg : buffSteamMomo,
+    menuName : "chicken fry Momo",
+    itemDescription : "Freshly Stream Buff Momos made from whole wheat and kneaded with beta Carotena Rich Vegetable Juice",
+    menuPrice : "200"
+  },
+  {
+    category: "chicken",
+    menuImg : buffSteamMomo,
+    menuName : "chicken chilly Momo",
+    itemDescription : "Freshly Stream Buff Momos made from whole wheat and kneaded with beta Carotena Rich Vegetable Juice",
+    menuPrice : "200"
+  },
+  {
+    category: "veg",
+    menuImg : buffSteamMomo,
+    menuName : "veg Steam Momo",
+    itemDescription : "Freshly Stream Buff Momos made from whole wheat and kneaded with beta Carotena Rich Vegetable Juice",
+    menuPrice : "200"
+  },
+  {
+    category: "veg",
+    menuImg : buffSteamMomo,
+    menuName : "veg fry Momo",
+    itemDescription : "Freshly Stream Buff Momos made from whole wheat and kneaded with beta Carotena Rich Vegetable Juice",
+    menuPrice : "200"
+  },
+  {
+    category: "veg",
+    menuImg : buffSteamMomo,
+    menuName : "veg chilly Momo",
+    itemDescription : "Freshly Stream Buff Momos made from whole wheat and kneaded with beta Carotena Rich Vegetable Juice",
+    menuPrice : "200"
+  }
+]
+
+
+const groupItemsByCategory = (items) => {
+  return items.reduce((accumulator, currentItem) => {
+    if (accumulator[currentItem.category]) {
+      accumulator[currentItem.category].push(currentItem);
+    } else {
+      accumulator[currentItem.category] = [currentItem];
+    }
+    return accumulator;
+  }, {});
+};
+
+
 const Menu = () => {
+
+
+  const groupedItems = groupItemsByCategory(menuItems);
+
+  const mappedItems = Object.entries(groupedItems).map(([category, items]) => ({
+    category,
+    items
+  }));
+
+
   return (
     <div className="mx-8 md:mx-16 xl:mx-32">
 
@@ -20,109 +110,26 @@ const Menu = () => {
       <QrScanner />
 
       <div className="py-4 mt-6 md:mt-12 md:py-6 flex flex-col gap-10">
-        <div className="flex flex-col gap-8">
-          <UniqueTitle 
-            primaryTitle="chicken" 
-            secondaryTitle={`momo's`} 
-          />
 
-          <div className="grid grid-cols-1 justify-items-center sm:justify-between sm:grid-cols-2 lg:grid-cols-3 lg:gap-12">
-            <MomoMenuList 
-              menuImg={buffSteamMomo}
-              menuName="buff steam momo"
-              menuPrice="200"
-              itemDescription="Freshly Stream Buff Momos made from whole wheat and kneaded with beta Carotena Rich Vegetable Juice"
-            />
-
-            <MomoMenuList 
-              menuImg={buffSteamMomo}
-              menuName="buff steam momo"
-              menuPrice="200"
-              itemDescription="Freshly Stream Buff Momos made from whole wheat and kneaded with beta Carotena Rich Vegetable Juice"
-            />
-
-            <MomoMenuList 
-              menuImg={buffSteamMomo}
-              menuName="buff steam momo"
-              menuPrice="200"
-              itemDescription="Freshly Stream Buff Momos made from whole wheat and kneaded with beta Carotena Rich Vegetable Juice"
-            />
-
-            <MomoMenuList 
-              menuImg={buffSteamMomo}
-              menuName="buff steam momo"
-              menuPrice="200"
-              itemDescription="Freshly Stream Buff Momos made from whole wheat and kneaded with beta Carotena Rich Vegetable Juice"
-            />
-
-            <MomoMenuList 
-              menuImg={buffSteamMomo}
-              menuName="buff steam momo"
-              menuPrice="200"
-              itemDescription="Freshly Stream Buff Momos made from whole wheat and kneaded with beta Carotena Rich Vegetable Juice"
-            />
-
-            <MomoMenuList 
-              menuImg={buffSteamMomo}
-              menuName="buff steam momo"
-              menuPrice="200"
-              itemDescription="Freshly Stream Buff Momos made from whole wheat and kneaded with beta Carotena Rich Vegetable Juice"
-            />
-
-          </div>
-        </div>
-
-        <div className="flex flex-col gap-8">
-          <UniqueTitle 
-            primaryTitle="buff" 
-            secondaryTitle={`momo's`} 
-          />
-
-          <div className="grid grid-cols-1 justify-items-center sm:justify-between sm:grid-cols-2 lg:grid-cols-3 lg:gap-12">
-            <MomoMenuList 
-              menuImg={buffSteamMomo}
-              menuName="buff steam momo"
-              menuPrice="200"
-              itemDescription="Freshly Stream Buff Momos made from whole wheat and kneaded with beta Carotena Rich Vegetable Juice"
-            />
-
-            <MomoMenuList 
-              menuImg={buffSteamMomo}
-              menuName="buff steam momo"
-              menuPrice="200"
-              itemDescription="Freshly Stream Buff Momos made from whole wheat and kneaded with beta Carotena Rich Vegetable Juice"
-            />
-
-            <MomoMenuList 
-              menuImg={buffSteamMomo}
-              menuName="buff steam momo"
-              menuPrice="200"
-              itemDescription="Freshly Stream Buff Momos made from whole wheat and kneaded with beta Carotena Rich Vegetable Juice"
-            />
-
-            <MomoMenuList 
-              menuImg={buffSteamMomo}
-              menuName="buff steam momo"
-              menuPrice="200"
-              itemDescription="Freshly Stream Buff Momos made from whole wheat and kneaded with beta Carotena Rich Vegetable Juice"
-            />
-
-            <MomoMenuList 
-              menuImg={buffSteamMomo}
-              menuName="buff steam momo"
-              menuPrice="200"
-              itemDescription="Freshly Stream Buff Momos made from whole wheat and kneaded with beta Carotena Rich Vegetable Juice"
-            />
-
-            <MomoMenuList 
-              menuImg={buffSteamMomo}
-              menuName="buff steam momo"
-              menuPrice="200"
-              itemDescription="Freshly Stream Buff Momos made from whole wheat and kneaded with beta Carotena Rich Vegetable Juice"
-            />
-
-          </div>
-        </div>
+            {
+              mappedItems.map(({ category, items }, index) => (
+              <div key={index} className="flex flex-col gap-8">
+                <UniqueTitle 
+                primaryTitle={category} 
+                secondaryTitle={`momo's`} 
+                />
+                <div className="grid grid-cols-1 justify-items-center sm:justify-between sm:grid-cols-2 lg:grid-cols-3 lg:gap-12">
+                  {items.map((item, index) => (
+                      <MomoMenuList key={index}
+                        menuImg={item.menuImg}
+                        menuName={item.menuName}
+                        menuPrice={item.menuPrice}
+                        itemDescription={item.itemDescription}
+                      />
+                  ))}
+                </div>
+              </div>
+            ))}
 
       </div>
 
