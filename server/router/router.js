999e93470323ec2c19c86b71c5916730f69427af
milangautamDemon <milangautam071440304@gmail.com>
2024-05-31 17:05:32 +0545

    refactor: router

--- server/router/router.js
+++ server/router/router.js
@@ -3,11 +3,13 @@ import addContact from "../controllers/addContactController.js"
 import contactValidator from "../helpers/contactValidator.js";
 import menuValidator from "../helpers/menuValidator.js";
 import addMenu from "../controllers/addMenuController.js";
+import getMenu from "../controllers/getMenuController.js";
 
 const router = express.Router();
 
 router.use("/add-contact", contactValidator, addContact);
-router.use("add-menu", menuValidator,  addMenu)
+router.use("add-menu", menuValidator,  addMenu);
+router.use("get-menu", getMenu);
 
 export {router};
 export default router;
